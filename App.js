b75bf4dd99e3ed4181d0510611ab1b6939e8eb8e
Luana Passos da Silva <luana60106@edu.unipar.br>
2026-04-23 22:03:14 -0300

    testando

--- App.js
+++ App.js
@@ -1,11 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View, Image, Alert, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [texto, setTexto] = useState('');
+
+  function testeExecutarBtn(){
+    Alert.alert('Botão clicado');
+  }
+
   return (
     <View style={styles.container}>
       <Text>Titulo</Text>
       <StatusBar style="auto" />
+      <View>
+        <Text>Tarefas</Text>
+        <TextInput onChangeText={setTexto} value={texto}/>
+        <Text>Valor</Text>
+        <TouchableOpacity onPress={testeExecutarBtn}>
+          <Text>Clique aqui</Text>
+        </TouchableOpacity>
+        <Button title='Clique aqui' onPress={testeExecutarBtn} />
+        <Image source={require('./assets/ToDoIcon.png')} style={{width: 50, height: 50}} />
+      </View>
     </View>
   );
 }
@@ -13,8 +30,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#d1f5d1',
+    backgroundColor: '#caddca',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+
